acde74ab5b3e964e4d0cb6f2cb90dcd05fbb1d98
quangquan4179 <quangquan4179@gmail.com>
2021-12-01 14:37:06 +0700

    test some template with materia-ui

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Auth/LoginComponent/Login';
 import DashboardNabar from './shares/layout/DashboardNabar';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './shares/theme';
+import RecipeReviewCard from './app/post';
 
 function App() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <DashboardNabar/>
+        <ThemeProvider theme={theme}> 
+             <div className="container">
+                <div className="row justify-content-center">
+                    <DashboardNabar/>
+                    <RecipeReviewCard/>
+                </div>
             </div>
-        </div>
+            
+        </ThemeProvider>
+       
     );
 }
 
